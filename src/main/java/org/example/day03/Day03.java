094098ff8f15d1c7e168435e89124be14ae147fe
ecti <dtworek94@gmail.com>
2024-12-03 16:04:45 +0000

    Completed day 02 part 02

--- src/main/java/org/example/day03/Day03.java
+++ src/main/java/org/example/day03/Day03.java
@@ -30,18 +30,19 @@ public class Day03 implements Day
             var choppedLine = line.toCharArray();
             for (int i = 0; i < choppedLine.length; i++) {
                 var length = 12;
-                if(choppedLine.length - i < 12) {
+                if (choppedLine.length - i < 12) {
                     length = choppedLine.length - i;
                 }
-                
                 var matchingString = String.copyValueOf(choppedLine, i, length);
+
+
                 if (matchingString.matches(regex)) {
                     var choppedMatch = matchingString.split(",");
                     var left = Long.valueOf(choppedMatch[0].split("\\(")[1]);
                     var right = Long.valueOf(choppedMatch[1].split("\\)")[0]);
 
                     result += mul(left, right);
-                } 
+                }
             }
         }
 
@@ -56,6 +57,44 @@ public class Day03 implements Day
     @Override
     public void solvePartTwo()
     {
+        boolean enabled = true;
+        long result = 0;
+        String mulRegex = "^mul\\(\\d{1,3},\\d{1,3}\\).*";
+        String dontRegex = "^don't\\(\\).*";
+        String doRegex = "^do\\(\\).*";
+
+
+        for (int y = 0; y < input.lines().size(); y++) {
+            var line = input.lines().get(y);
+            var choppedLine = line.toCharArray();
+            for (int i = 0; i < choppedLine.length; i++) {
+                var length = 12;
+                if (choppedLine.length - i < 12) {
+                    length = choppedLine.length - i;
+                }
+                var matchingString = String.copyValueOf(choppedLine, i, length);
+
+                if (matchingString.matches(doRegex)) {
+                    enabled = true;
+                }
+                
+                if(matchingString.matches(dontRegex)) {
+                    enabled = false;
+                }
+
+                if (matchingString.matches(mulRegex)) {
+                    var choppedMatch = matchingString.split(",");
+                    var left = Long.valueOf(choppedMatch[0].split("\\(")[1]);
+                    var right = Long.valueOf(choppedMatch[1].split("\\)")[0]);
+
+                    if (enabled) {
+                        result += mul(left, right);
+                    }
+                }
+            }
+        }
+
+        System.out.println(result);
 
     }
 }
